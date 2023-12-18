43ae5a9c198afbdaad28b7a2f55b149ef89fbc41
galan0455 <galun2004@gmail.com>
2023-12-18 12:36:13 +0100

    18/12/2023

--- dia37/acceso_mysql2.php
+++ dia37/acceso_mysql2.php
@@ -1,9 +1,10 @@
 <?php
   //Configuracion de la conexion a base de datos
-  $bd_host = "mysql";
-  $bd_usuario = "usuario";
-  $bd_password = "1234567";
-  $bd_base = "2daw";
+  $bd_host = $_ENV["BBDD_HOST"];
+  $bd_usuario =$_ENV["BBDD_USER"];
+  $bd_password = $_ENV["BBDD_PASSWORD"];
+  $bd_base = $_ENV["BBDD_DATABASE"];
+  $bd_port = $_ENV["BBDD_HOST_PORT"];
 
   try {
     $dsn = "mysql:host=$bd_host;dbname=$bd_base";
