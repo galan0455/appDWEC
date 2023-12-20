019482abbc36783c44e94ad88ce3ec371c6475a2
galan0455 <galun2004@gmail.com>
2023-12-20 15:27:03 +0100

    20/12/2023

---
+++ bingo/login.php
@@ -0,0 +1,14 @@
+<?php
+    $usuario = $_POST["usuario"];
+    $clave = $_POST["clave"];
+    
+    if($usuario=="juan" && $clave=="1234"){
+        session_start();
+        $_SESSION["usuario"]=$usuario;
+        echo '{"estado":"OK"}';
+    }else {
+        echo '{"estado":"Error"}';
+        
+    }
+
+?>
